bb0550d8704ab13f6b4a0707a03f60fe50726fbd
MarcusCastilhos <marcus.drummer@live.com>
2024-10-31 23:12:22 -0300

    feat: testing git changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import { createServer } from "node:http";
 
 const server = createServer((request, response) => {
-  response.write("hello world");
+  response.write("hello world1");
   return response.end();
 });
 
